13db6f6dee2db2a3313c17442078c68daab3e79c
Marcel Oborski <73552729+lakamakafon@users.noreply.github.com>
2021-04-07 10:11:19 +0200

    Dodanie nowych tras routingu

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/logout', function () {
+    Session::forget('user');
+    return redirect('/login');
+});
+
+Route::post("/login", [UserController::class, 'login']);
+Route::get("/", [ProductController::class, 'index']);
+Route::get("/detail/{id}", [ProductController::class, 'detail']);
+Route::get("/search", [ProductController::class, 'search']);
+Route::post("/add_to_cart", [ProductController::class, 'addToCart']);
+Route::get("/cartlist", [ProductController::class, 'cartlist']);
+Route::get("/removecart/{id}", [ProductController::class, 'removeCart']);
